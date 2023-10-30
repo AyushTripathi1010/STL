b99754251aaceb60f2a9e20100e5fa4d696c2f1c
Ayush Tripathi <74903504+AyushTripathi1010@users.noreply.github.com>
2023-10-30 18:48:35 +0530

    STL Functions

--- STL.cpp
+++ STL.cpp
@@ -1,7 +1,9 @@
 
 
 // Method 1: using Lamda expression
-
+   // the [] used in lamda is called capture claus which is used for capturing a variable or function which is defined 
+   // outside the lamda block. TO access them we use [&] ampersand sign , other wise we can leave it empty, if we don't
+   // want to access anything from outside.
  			auto lamda = [&](string &w1 , string &w2)
               {
                 return  w1.size() < w2.size();
@@ -29,3 +31,20 @@
              
              
       		 sort(words.begin() , words.end() , comp);
+
+
+// How to count number of set bits in a number using STL functions in O(1) time.
+ int bits_num1 = __builtin_popcount(num1);    // Eg: 6 = 110 => bits_num1 = 2 , because 2 set bits.
+
+// TO calculate gcd of  numbers, we can usse:
+ int gcd = __gcd(num1, num2);
+
+// To calculate the lower and upper boud  of a number in sorted array, we use
+
+ int lb = lower_bound(arr.begin() , arr.end(), target) - arr.begin(); // when we subtract arr.begin() from the lower_bound it gives us the 
+// index of the target.
+ int ub = upper_bound(arr.begin() , arr.end(), target) - arr.begin();
+
+ 
+
+
